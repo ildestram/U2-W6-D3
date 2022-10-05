e493cb76180f989fb5653a1a545e619250ac5134
Ilde Stramandinoli <109019707+ildestram@users.noreply.github.com>
2022-10-05 17:45:22 +0200

    .

--- js/script.js
+++ js/script.js
@@ -3,20 +3,21 @@
 //     img.setAttribute("src", fileName);
 // }
 
-// let myImage = document.querySelector('img');
+let myImage = document.querySelector('img');
 
-// myImage.onclick = function() {
-//     let mySrc = myImage.getAttribute('src');
-//     if(mySrc === "./js/first.jpeg") {
-//         myImage.setAttribute('src', "./js/locandina.jpeg");
-//     } else {
-//         myImage.setAttribute('src', "./js/first.jpeg");
-//     }
-// }
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === "./js/first.jpeg") {
+        myImage.setAttribute('src', "./js/locandina.jpeg");
+    } else {
+        myImage.setAttribute('src', "./js/first.jpeg");
+    }
+}
 
-let myBtn = document.getElementById("myBtn");
-let image = 
+// let myBtn = document.getElementById("myBtn");
+// let image = 
 
-myBtn.addEventListener("click", function(e) {
-    document.body.innerHTML += 
-})
+// myBtn.addEventListener("click", function(e) {
+//     document.body.innerHTML += 
+// })
+
